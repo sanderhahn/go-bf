cf2609666dbe1eca7d641d0772435cb89f61c48d
Sander Hahn <sanderhahn@gmail.com>
2019-12-22 14:23:28 +0100

    test population

---
+++ population_test.go
@@ -0,0 +1,31 @@
+package bf
+
+import "testing"
+
+func RunPopulation(expected string, maxRuntime, maxIterations int) *Population {
+	p := NewPopulation()
+	p.Expected = expected
+	p.MaxRuntime = maxRuntime
+	for i := 1; i < maxIterations; i++ {
+		p.EvaluateAndMutate()
+		if p.Fittest().success {
+			break
+		}
+	}
+	return p
+}
+
+func TestHiPopulation(t *testing.T) {
+	hi := RunPopulation("hi\n", 200, 100)
+	_ = hi.Fittest().String()
+	if _, ok := hi.FittestCode(); !ok {
+		t.Fail()
+	}
+}
+
+func TestHelloWorldPopulation(t *testing.T) {
+	p := RunPopulation("hello world\n", 1000, 500)
+	if _, ok := p.FittestCode(); !ok {
+		t.Fail()
+	}
+}
